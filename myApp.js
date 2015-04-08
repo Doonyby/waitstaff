cc5ca01727affed57298453615669f0d30015e6a
iris-qu <iris.xiaoyu.qu@gmail.com>
2015-04-07 23:26:01 -0700

    Adding routes
    Installing bower

--- myApp.js
+++ myApp.js
@@ -1,29 +1,43 @@
 
-var app = angular.module('myApp', [])
-	
-app.controller('MyCtrl', function($scope){
-	$scope.Math = window.Math;
+angular.module('myApp', ['ngRoute', 'ngAnimate'])
+	.config(['$routeProvider', function($routeProvider){
+		$routeProvider.when('/', {
+			templateUrl: 'home.html',
+			controller: 'MyCtrl'
+		})
+		.when('/calculator', {
+			templateUrl: 'calculator.html',
+			controller: 'MyCtrl'
+		})
+		.when('/summary', {
+			templateUrl: 'summary',
+			controller: 'MyCtrl'
+		})
+	}])
+	.controller('MyCtrl', function($scope){
 
-	$scope.tip_total = 0; 
-	$scope.count = 0;
-	$scope.tip = 0;
-	$scope.subtotal = 0;
-	$scope.total = 0;
-	$scope.avg = 0;
+		$scope.Math = window.Math;
 
-	$scope.calculate = function() {   
+		$scope.tip_total = 0; 
+		$scope.count = 0;
+		$scope.tip = 0;
+		$scope.subtotal = 0;
+		$scope.total = 0;
+		$scope.avg = 0;
 
-		$scope.tip = $scope.price * $scope.tip_p * 0.01;
-		$scope.subtotal = $scope.price * $scope.tax_p * 0.01 + $scope.price;
-	    $scope.count = $scope.count + 1;
-	    $scope.tip_total = $scope.tip_total + $scope.tip;
-	    $scope.total = $scope.tip + $scope.subtotal;
-	    $scope.avg = $scope.tip_total / $scope.count; 
-        
-    };
+		$scope.calculate = function() {   
 
-   
-});
+			$scope.tip = $scope.price * $scope.tip_p * 0.01;
+			$scope.subtotal = $scope.price * $scope.tax_p * 0.01 + $scope.price;
+		    $scope.count = $scope.count + 1;
+		    $scope.tip_total = $scope.tip_total + $scope.tip;
+		    $scope.total = $scope.tip + $scope.subtotal;
+		    $scope.avg = $scope.tip_total / $scope.count; 
+	        
+	    };
+
+	   
+	});
 
 
 		
