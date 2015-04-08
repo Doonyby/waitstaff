ec2107e4a8df909e20f96c283b8403ac8bf399dc
iris-qu <iris.xiaoyu.qu@gmail.com>
2015-04-08 14:18:20 -0700

    Adding factory

--- myApp.js
+++ myApp.js
@@ -4,42 +4,63 @@ angular.module('myApp', ['ngRoute', 'ngAnimate'])
 		$routeProvider.when('/', {
 			templateUrl: 'home.html',
 			
+			
 		})
 		.when('/calculator', {
 			templateUrl: 'calculator.html',
-			controller: 'MyCtrl'
+			controller: 'tipCtrl'
 		})
 		.when('/summary', {
 			templateUrl: 'summary',
-			controller: 'MyCtrl'
+			controller: 'summaryCtrl'
 		})
 	}])
-	.controller('MyCtrl', function($scope){
+	.factory('info', function(){
+		return {
+			data: {
+				price: 0,
+				tax_p: 0,
+				tip_p: 0,
+				count: 0,
+				tip_total: 0,
+				subtotal: 0,
+				tip: 0,
+				avg: 0,
+			}
+		}
+	})
+	.controller('tipCtrl', function($scope, info){
 
-		$scope.Math = window.Math;
-
-		$scope.tip_total = 0; 
-		$scope.count = 0;
 		$scope.tip = 0;
 		$scope.subtotal = 0;
 		$scope.total = 0;
-		$scope.avg = 0;
+		
 
 		$scope.calculate = function() {   
 
 			$scope.tip = $scope.price * $scope.tip_p * 0.01;
-			$scope.subtotal = $scope.price * $scope.tax_p * 0.01 + $scope.price;
-		    $scope.count = $scope.count + 1;
-		    $scope.tip_total = $scope.tip_total + $scope.tip;
+			$scope.subtotal = $scope.price * $scope.tax_p * 0.01 + $scope.price; 
 		    $scope.total = $scope.tip + $scope.subtotal;
-		    $scope.avg = $scope.tip_total / $scope.count; 
-	        
-	    };
-
+		   	        
+	    }
 	   
+	})
+	.controller('summaryCtrl', function($scope, info){
+
+		$scope.tip_total = 0; 
+		$scope.count = 0;
+		$scope.avg = 0;
+
+		$scope.update = function() {
+
+			$scope.avg = $scope.tip_total / $scope.count; 
+			$scope.count = $scope.count + 1;
+		    $scope.tip_total = $scope.tip_total + $scope.tip;
+		}
+
 	});
 
 
-		
 
+ 
 
