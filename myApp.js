cc1cef6748655ff4f7a6a7def4bd9cd21b36f028
iris-qu <iris.xiaoyu.qu@gmail.com>
2015-04-10 15:22:32 -0700

    Fully functioning

--- myApp.js
+++ myApp.js
@@ -1,4 +1,3 @@
-
 angular.module('myApp', ['ngRoute', 'ngAnimate'])
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider.when('/', {
@@ -12,7 +11,7 @@ angular.module('myApp', ['ngRoute', 'ngAnimate'])
 		})
 		.when('/summary', {
 			templateUrl: 'summary',
-			controller: 'summaryCtrl'
+			controller: 'tipCtrl'
 		})
 	}])
 	.factory('info', function(){
@@ -26,39 +25,55 @@ angular.module('myApp', ['ngRoute', 'ngAnimate'])
 				subtotal: 0,
 				tip: 0,
 				avg: 0,
-				total: 0,
+				total:0
 			}
 		}
 	})
-	.controller('tipCtrl', function($scope, info){
+	.controller('tipCtrl', function($scope, $route, info, $location){
 
 		$scope.tip = info.data.tip;
 		$scope.subtotal = info.data.subtotal;
 		$scope.total = info.data.total;
-		
 
+		$scope.tip_total = info.data.tip_total;
+		$scope.count = info.data.count;
+		$scope.avg = info.data.avg;
+		
 		$scope.calculate = function() {   
 
 			info.data.tip = $scope.price * $scope.tip_p * 0.01;
 			info.data.subtotal = $scope.price * $scope.tax_p * 0.01 + $scope.price; 
-		    info.data.total = $scope.tip + $scope.subtotal;
-		   	        
+		    info.data.total = info.data.tip + info.data.subtotal
+
+		    info.data.tip_total = info.data.tip_total + info.data.tip;
+		    info.data.count = info.data.count + 1;
+		    info.data.avg = info.data.tip_total / info.data.count; 
+
+		    $location.path('calculator');
+		    $route.reload();
+
 	    }
-	   
-	})
-	.controller('summaryCtrl', function($scope, info){
 
-		$scope.tip_total = info.data.tip_total; 
-		$scope.count = info.data.count;
-		$scope.avg = info.data.avg;
-
-		$scope.update = function() {
-
-			info.data.avg = $scope.tip_total / $scope.count; 
-			info.data.count = $scope.count + 1;
-		    info.data.tip_total = $scope.tip_total + $scope.tip;
+	    $scope.reset = function(){
+	    	info.data.tip=0;
+	    	info.data.subtotal=0;
+	    	info.data.total=0;
+			info.data.count = 0;
+			info.data.tip_total = 0;
+			info.data.avg = 0;
+			this.update();
 		}
 
+		$scope.update = function(){
+			$scope.tip = info.data.tip;
+			$scope.subtotal = info.data.subtotal;
+			$scope.total = info.data.total;
+			$scope.tip_total = info.data.tip_total;
+			$scope.count = info.data.count;
+			$scope.avg = info.data.avg;
+		}
+
+	   
 	});
 
 
